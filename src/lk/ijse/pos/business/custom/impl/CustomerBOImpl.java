9fa33a3c5b5ca626b0785e016a9ab568f6fcae5d
DulminiWijekoon <dulminiwijekoon@gmail.com>
2019-05-29 09:37:51 +0530

    Commit the project

---
+++ src/lk/ijse/pos/business/custom/impl/CustomerBOImpl.java
@@ -0,0 +1,76 @@
+package lk.ijse.pos.business.custom.impl;
+
+import lk.ijse.pos.business.custom.CustomerBO;
+import lk.ijse.pos.dao.DAOFactory;
+import lk.ijse.pos.dao.DAOTypes;
+import lk.ijse.pos.dao.custom.CustomerDAO;
+import lk.ijse.pos.db.HibernateUtil;
+import lk.ijse.pos.dto.CustomerDTO;
+import lk.ijse.pos.entity.Customer;
+import org.hibernate.Session;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomerBOImpl implements CustomerBO {
+
+    private CustomerDAO customerDAO = DAOFactory.getInstance().getDAO(DAOTypes.CUSTOMER);
+
+//    public CustomerBOImpl(){
+//        String dao = DAOFactory.getInstance().<String>getDAO(DAOTypes.CUSTOMER);
+//    }
+
+    @Override
+    public CustomerDTO getCustomerById(String id) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+
+        em.getTransaction().begin();
+        customerDAO.setSession(em);
+        Customer customer = customerDAO.find(id);
+        CustomerDTO customerDTO = new CustomerDTO(customer.getId(), customer.getName(), customer.getAddress());
+        em.getTransaction().commit();
+        em.close();
+        return customerDTO;
+
+    }
+
+    public List<CustomerDTO> getAllCustomers() throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        customerDAO.setSession(em);
+        List<CustomerDTO> customers = customerDAO.findAll().stream().map(customer -> new CustomerDTO(customer.getId(), customer.getName(), customer.getAddress())).collect(Collectors.toList());
+        em.getTransaction().commit();
+        em.close();
+        return customers;
+
+    }
+
+    public void saveCustomer(CustomerDTO dto) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        customerDAO.setSession(em);
+        customerDAO.save(new Customer(dto.getId(), dto.getName(), dto.getAddress()));
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void updateCustomer(CustomerDTO dto) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        customerDAO.setSession(em);
+        customerDAO.update(new Customer(dto.getId(), dto.getName(), dto.getAddress()));
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void removeCustomer(String id) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        customerDAO.setSession(em);
+        customerDAO.delete(id);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+}
---
+++ src/lk/ijse/pos/business/custom/impl/ItemBOImpl.java
@@ -0,0 +1,59 @@
+package lk.ijse.pos.business.custom.impl;
+
+import lk.ijse.pos.business.custom.ItemBO;
+import lk.ijse.pos.dao.DAOFactory;
+import lk.ijse.pos.dao.DAOTypes;
+import lk.ijse.pos.dao.custom.ItemDAO;
+import lk.ijse.pos.db.HibernateUtil;
+import lk.ijse.pos.dto.ItemDTO;
+import lk.ijse.pos.entity.Item;
+import org.hibernate.Session;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ItemBOImpl implements ItemBO {
+
+    private ItemDAO itemDAO = DAOFactory.getInstance().getDAO(DAOTypes.ITEM);
+
+    public List<ItemDTO> getAllItems() throws Exception {
+
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        itemDAO.setSession(em);
+        List<ItemDTO> items = itemDAO.findAll().stream().map(item -> new ItemDTO(item.getCode(), item.getDescription(), item.getUnitPrice(), item.getQtyOnHand())).collect(Collectors.toList());
+        em.getTransaction().commit();
+        em.close();
+        return items;
+
+    }
+
+    public void saveItem(ItemDTO item) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        itemDAO.setSession(em);
+        itemDAO.save(new Item(item.getCode(), item.getDescription(), item.getUnitPrice(), item.getQtyOnHand()));
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void updateItem(ItemDTO item) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        itemDAO.setSession(em);
+        itemDAO.update(new Item(item.getCode(), item.getDescription(), item.getUnitPrice(), item.getQtyOnHand()));
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void deleteItem(String code) throws Exception {
+        EntityManager em = HibernateUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        itemDAO.setSession(em);
+        itemDAO.delete(code);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+}
